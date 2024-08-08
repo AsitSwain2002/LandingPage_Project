f31518e60f283c242a7feff776356352e3f2f3c2
AsitSwain2002 <asitswain7327@gmil.com>
2024-08-08 17:18:41 +0530

    add functionality

--- Js/script.js
+++ Js/script.js
@@ -2,6 +2,10 @@ const signIn = document.querySelector(".signIn");
 const mainCon = document.querySelector(".mainContainer");
 const cut = document.querySelector(".ri-close-fill");
 const closeLetter = document.querySelector(".closeLetter");
+const submit = document.querySelector("#form");
+const emailVal = document.querySelector("#email");
+const theme = document.querySelector(".signup");
+const mainCont = document.querySelector(".mainCon");
 
 signIn.addEventListener('click' , ()=>{
 mainCon.style.display = "block";
@@ -10,7 +14,12 @@ mainCon.style.display = "block";
 cut.addEventListener("click" , ()=>{
     mainCon.style.display = "none"
 })
-
+submit.addEventListener("submit" , (e)=>{
+    e.preventDefault();
+    // const pass = document.querySelector("#pas").value;
+    // const maskedPass = pass.length > 3 ? 'X'.repeat(pass.length - 3) + pass.slice(-3) : pass.value = "X";
+    // alert(`Email : ${emailVal.value}\nPassword : ${maskedPass}`)
+})
 // cut.addEventListener("mouseenter", () => {
 //     closeLetter.style.display = "block";
 // });
@@ -18,3 +27,16 @@ cut.addEventListener("click" , ()=>{
 // cut.addEventListener("mouseleave", () => {
 //     closeLetter.style.display = "none";
 // });
+
+theme.addEventListener('click',()=>{
+
+    if(theme.innerText === 'Dark'){
+    mainCont.style.background = "black";
+    mainCont.style.color = "white";
+    theme.innerText = "Light"
+    }else{
+        mainCont.style.background = "linear-gradient(-145deg, rgb(23, 93, 105) 25%, rgb(51, 12, 67) 95%)";
+        mainCont.style.color = "white";
+        theme.innerText = "Dark"
+    }
+})
